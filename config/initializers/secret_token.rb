8557ef4153c85379a1e36e335f9aa9ab6920737f
smiller <sean@seanmiller.ca>
2013-05-18 16:32:00 -0400

    Initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Angelhack2013Drip::Application.config.secret_token = '7e1e192b7a2290d0167687f02e22cbcf489bf1f266c67eac2bb73b6eedde2343268a16c5fa1528419c1d2203b4488eaa54cb56d6f96745483bd8c43c8647dbbb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Angelhack2013Drip::Application.config.session_store :cookie_store, :key => '_angelhack2013-drip_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Angelhack2013Drip::Application.config.session_store :active_record_store
