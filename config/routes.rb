84b33dbbb5057f956760f0d444adfb24c7c7a456
smiller <sean@seanmiller.ca>
2013-05-18 17:16:53 -0400

    fix routes typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Angelhack2013Drip::Application.routes.draw do
-
   root :to => 'home#index'
-
   match '/auth/twitter/callback' => 'sessions_twitter#create'
   match '/signout_twitter' => 'sessions_twitter#destroy'
   match '/signin_twitter' => 'sessions_twitter#new'
-
   match '/auth/facebook/callback' => 'sessions_facebook#create'
   match '/signout_facebook' => 'sessions_facebook#destroy'
-  match '/signin_facebook' => 'sessions_facebook#new
-
+  match '/signin_facebook' => 'sessions_facebook#new'
 end
