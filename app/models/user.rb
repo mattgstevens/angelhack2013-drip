08f1e86faab81b455ef28c4e7b1203bb2e207b7f
smiller <sean@seanmiller.ca>
2013-05-18 21:33:57 -0400

    Add user.tweet(msg)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ActiveRecord::Base
   has_many :user_auths
   has_many :auths, through: :user_auths
 
+  def tweet(msg)
+    tweeter = Twitter::Client.new(
+      oauth_token:, current_user.auth.token
+      oauth_token_secret: current_user.auth.secret
+    )
+    tweeter.update(msg)
+  end
+
   def self.create_with_omniauth_facebook(auth_params)
     user = User.new
     if auth_params['info']
