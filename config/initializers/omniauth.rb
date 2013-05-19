86792452d19082b5f45619b662045446fae481a9
smiller <sean@seanmiller.ca>
2013-05-19 03:07:26 -0400

    Tweak facebook scope in config/initializers/omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-  :scope => 'email,user_birthday,read_stream,publish_actions'
+  :scope => 'email,publish_actions'
 end
